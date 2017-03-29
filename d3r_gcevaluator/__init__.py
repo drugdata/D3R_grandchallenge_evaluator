16d710c69f70f0214ee28aa08f4d14f7c1e394ff
Chris Churas <churas@gmail.com>
2017-03-29 12:44:21 -0700

    Revert "initial revision"
    This reverts commit 2f02667a64cf24c189a15b84a39a07eb176df76f.

--- d3r_gcevaluator/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = """Shuail Liu"""
-__email__ = 'shuailiu25@gmail.com'
-__version__ = '0.1.1'
--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use D3R Grand Challenge Evaluator in a project::
-
-    import d3r_gcevaluator
